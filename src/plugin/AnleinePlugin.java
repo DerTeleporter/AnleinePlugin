c66e3fa8423f8b61099341def255951b9df45d76
DerTeleporter <xphilipx200@gmail.com>
2016-05-22 10:21:06 +0200

    dfg

--- src/plugin/AnleinePlugin.java
+++ src/plugin/AnleinePlugin.java
@@ -5,7 +5,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class AnleinePlugin extends JavaPlugin {
 
 	public void onEnable() {
-		System.out.println("[Anleine]Das Plugin wurde gestartet");
+		System.out.println("[Anleine]Das Plugin wurde gestartet...");
 
 		getServer().getPluginManager().registerEvents(new AnleineListener(),
 				this);
